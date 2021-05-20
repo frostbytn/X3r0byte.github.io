2c9d34132393ff701cefa5fa8edaa7b21bf1246c
X3r0byte <41211085+X3r0byte@users.noreply.github.com>
2021-05-20 00:35:32 -0500

    mass update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
-function resume(){
+
+function resume() {
     window.location = './resume.html';
+}
+
+function loadASTR() {
+    gameInstance = UnityLoader.instantiate("gameContainer", "Build/build.json", { onProgress: UnityProgress });
+}
+
+function fullscreen() {
+
+    if (gameInstance) {
+        gameInstance.SetFullscreen(1);
+    }
+    else {
+        gameInstance = UnityLoader.instantiate("gameContainer", "Build/build.json", { onProgress: UnityProgress });
+        gameInstance.SetFullscreen(1);
+    }
+}
+
+function sendMessage() {
+
+    var from = document.getElementById('contact-from').value;
+    var email = document.getElementById('contact-email').value;
+    var message = document.getElementById('contact-message').value;
+
+    // please tell me if you found this :)
+    emailjs.init("user_kycrFg3YsSi26CWoPkLSP");
+    
+    var templateParams = {
+        from_name: from,
+        message: message,
+        reply_to: email
+    };
+    emailjs.send('service_eey6yvh', 'template_xzyicsh', templateParams)
+        .then(function (response) {
+            $("#contactModal").modal()
+            console.log('SUCCESS! Your message was sent.', response.status, response.text);
+        }, function (error) {
+            window.location = './index.html';
+            alert('For some reason, the email service (emailjs) failed to send your message. Please use one of my listed emails to contact me, sorry for the inconvenience :(')
+            console.log('FAILED...', error);
+        });
 }
