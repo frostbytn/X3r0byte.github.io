de6ca9e2c2b9e0f1b82323f510a9459f2ffdc6d6
Gary <frostbyteultra@outlook.com>
2024-12-09 22:56:17 -0600

    add some effects

--- script.js
+++ script.js
@@ -16,6 +16,27 @@ var messagePos = 0;
 
 
 $(document).ready(function () {
+
+    const faders = document.querySelectorAll('.fade-in');
+    const options = {
+      threshold: 0.1
+    };
+    
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if(entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        } else {
+          entry.target.classList.remove('visible');
+        }
+      });
+    }, options);
+    
+    faders.forEach(fader => {
+      observer.observe(fader);
+    });
+    
+
     _text = document.getElementById('greeting-text').innerText;
     message = document.getElementById('message-text').innerText;
     var cursor = document.getElementById('txt-cursor');
