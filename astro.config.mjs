1cfa57c89c716836a19924b58d171b2cc508f894
Gary <41211085+frostbytn@users.noreply.github.com>
2026-03-10 09:18:09 -0500

    Revert "add some seo and social helpers"
    This reverts commit 1cad189bafe43a6efa09c5a4ed258fc1f9f835a8.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   srcDir: './astro',
   site: 'https://garymckeever.us',
-  output: 'static',
-  integrations: [sitemap()]
+  output: 'static'
 });
